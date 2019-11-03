1a5e933d75c5c1ab6435ee212e3817c3cf44e671
banjianpan <40034920+banjianpan@users.noreply.github.com>
2019-11-03 11:53:58 -0600

    hello2

--- ChangeTree/app/src/main/java/bdp839/mobile/changetree/curCharities.kt
+++ ChangeTree/app/src/main/java/bdp839/mobile/changetree/curCharities.kt
@@ -1,15 +1,28 @@
 package bdp839.mobile.changetree
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.cur_charities.*
+import kotlinx.android.synthetic.main.cur_charities.view.*
+
 
 class curCharities: Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
             View? = inflater.inflate(R.layout.cur_charities, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.getInfo.setOnClickListener({
+            Log.d("curCharities", "inside clicked")
+            activity!!.supportFragmentManager.beginTransaction().add(R.id.cur_change_container, openInfo.newInstance()).addToBackStack(null).commit()
+        })
+    }
 
     companion object{
         fun newInstance(): curCharities = curCharities()
--- ChangeTree/app/src/main/java/bdp839/mobile/changetree/openInfo.kt
+++ ChangeTree/app/src/main/java/bdp839/mobile/changetree/openInfo.kt
@@ -1,4 +1,19 @@
 package bdp839.mobile.changetree
 
-class openInfo {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class openInfo: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.open_info, container, false)
+    }
+
+    companion object{
+        fun newInstance(): openInfo{
+            return openInfo()
+        }
+    }
 }
